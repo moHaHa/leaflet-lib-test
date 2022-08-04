943ed33f86d39422967abe5460001683442b4312
HBO30 <diab.4869@gmail.com>
2022-08-04 17:03:52 +0300

    LLT-7[finish : Quick Start with leaflet library]

--- src/modules/quick-start/pages/test/test-page.vue
+++ src/modules/quick-start/pages/test/test-page.vue
@@ -1,13 +1,43 @@
 <template>
-  <v-sheet min-height="70vh" rounded="lg">
-   <div class="py-3 px-3">
-    <div class="text--h1">Hello test</div>
-   </div>
-  </v-sheet>
+  <div class="test-page">
+    <v-sheet min-height="70vh" rounded="lg">
+      <div class="py-3 px-3 h-100">
+        <div id="map" class="grey"></div>
+      </div>
+    </v-sheet>
+  </div>
 </template>
 
 <script>
-export default {};
+import L from "leaflet";
+export default {
+  data() {
+    return {
+      L,
+      map: undefined,
+    };
+  },
+  methods: {
+    makeMap() {
+      this.map = L.map("map").setView([33.5244122,36.2872257] , 19);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 22,
+        attribution: "© OpenStreetMap",
+      }).addTo(this.map);
+    },
+  },
+  mounted() {
+    this.makeMap();
+  },
+};
 </script>
 
-<style></style>
+<style lang="scss">
+@import '~leaflet/dist/leaflet.css';
+.test-page {
+  #map {
+    height: 400px;
+    overflow: hidden;
+  }
+}
+</style>
