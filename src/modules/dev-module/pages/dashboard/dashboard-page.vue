0c68097a375df38ef32c3db356c573fa9731047a
HBO30 <diab.4869@gmail.com>
2022-08-05 22:33:36 +0300

    LLT-9[dashboard]

--- src/modules/dev-module/pages/dashboard/dashboard-page.vue
+++ src/modules/dev-module/pages/dashboard/dashboard-page.vue
@@ -1,80 +1,28 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      app
-      color="white"
-      flat
-    >
-      <v-container class="py-0 fill-height">
-        <v-avatar
-          class="mr-10"
-          color="grey darken-1"
-          size="32"
-        ></v-avatar>
+    <v-app-bar app shrink-on-scroll>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-        <v-btn
-          v-for="link in links"
-          :key="link"
-          text
-        >
-          {{ link }}
-        </v-btn>
+      <v-toolbar-title>App Development env</v-toolbar-title>
 
-        <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-        <v-responsive max-width="260">
-          <v-text-field
-            dense
-            flat
-            hide-details
-            rounded
-            solo-inverted
-          ></v-text-field>
-        </v-responsive>
-      </v-container>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-app-bar>
 
-    <v-main class="grey lighten-3">
+    <v-main>
       <v-container>
         <v-row>
-          <v-col cols="2">
-            <v-sheet rounded="lg">
-              <v-list color="transparent">
-                <v-list-item
-                  v-for="n in 5"
-                  :key="n"
-                  link
-                >
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      List Item {{ n }}
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-
-                <v-divider class="my-2"></v-divider>
-
-                <v-list-item
-                  link
-                  color="grey lighten-4"
-                >
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      Refresh
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list>
-            </v-sheet>
-          </v-col>
-
-          <v-col>
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
-              <!--  -->
-            </v-sheet>
+          <v-col cols="4" xl="3" v-for="(link, index) in links" :key="index">
+            <v-card height="180">
+              <router-link :to="link.link">
+                <v-img cover position="start" class="h-100" :src="link.img">
+                </v-img>
+              </router-link>
+              <v-card-title>{{link.title}} </v-card-title>
+            </v-card>
           </v-col>
         </v-row>
       </v-container>
@@ -83,14 +31,23 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
+export default {
+  data() {
+    return {
       links: [
-        'Dashboard',
-        'Messages',
-        'Profile',
-        'Updates',
+        {
+          link: "quick-start/test",
+          img: "https://github.com/HBO30/public/blob/images/images/quick-start-leaflet-test.jpg?raw=true",
+          title: "Quick start",
+        },
+        {
+          link: "jeojson/preview",
+          img: "https://github.com/HBO30/public/blob/images/images/leaf-let-jeojson-preview.jpg?raw=true",
+          title: "Jeo JSON",
+        },
       ],
-    }),
-  }
+    };
+  },
+  //
+};
 </script>
