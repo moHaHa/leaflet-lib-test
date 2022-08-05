cfb8c493b8b7f6e676e5faf71f88e9e319044dd3
HBO30 <diab.4869@gmail.com>
2022-08-05 23:55:35 +0300

    LLT-9[qucik-start module > update layout]

--- src/modules/quick-start/layout/index-layout.vue
+++ src/modules/quick-start/layout/index-layout.vue
@@ -1,39 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      app
-      color="white"
-      flat
-    >
-      <v-container class="py-0 fill-height">
-        <v-avatar
-          class="mr-10"
-          color="grey darken-1"
-          size="32"
-        ></v-avatar>
-
-        <v-btn
-          v-for="link , index in links"
-          :key="index"
-          text
-          :to="link.link"
-        >
-          {{ link.text }}
-        </v-btn>
-
-        <v-spacer></v-spacer>
-
-        <v-responsive max-width="260">
-          <v-text-field
-            dense
-            flat
-            hide-details
-            rounded
-            solo-inverted
-          ></v-text-field>
-        </v-responsive>
-      </v-container>
-    </v-app-bar>
+    <mh-module-nav-bar :links="links" />
 
     <v-main class="grey lighten-3">
       <v-container>
