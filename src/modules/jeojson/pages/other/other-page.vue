958c7f4c51aeea7ec9bfa32ea9bac9b56a9cb78a
HBO30 <diab.4869@gmail.com>
2022-08-05 22:37:58 +0300

    LLT-8[other page]

---
+++ src/modules/jeojson/pages/other/other-page.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-sheet min-height="70vh" rounded="lg">
+   <div class="py-3 px-3">
+    <div class="text--h1">Hello other page</div>
+   </div>
+  </v-sheet>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
