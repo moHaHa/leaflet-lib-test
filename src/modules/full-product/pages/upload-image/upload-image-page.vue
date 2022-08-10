f124ee41bbbee15976cccd7b4ea738a88a42d912
HBO30 <diab.4869@gmail.com>
2022-08-10 22:07:02 +0300

    LLT-23

--- src/modules/full-product/pages/upload-image/upload-image-page.vue
+++ src/modules/full-product/pages/upload-image/upload-image-page.vue
@@ -1,14 +1,18 @@
 <template>
   <v-app class="upload-image-page">
-    <navigation :links="[]" >
-    <v-btn v-if="mapImageUrl"  text @click="$vuetify.goTo('/full-product/preview')">
-        <span class="mr-2">Next</span>
+    <navigation :links="[]">
+      <v-btn
+        v-if="mapImageUrl"
+        text
+        @click="$router.push({ path: '/full-product/preview' })"
+      >
+        <span class="mr-2">Preview</span>
       </v-btn>
     </navigation>
     <v-main class="upload-image-page-MODEL-main">
       <v-container class="my-8">
         <v-row>
-          <v-col  cols="6" offset="3" >
+          <v-col cols="6" offset="3">
             <v-file-input
               dark
               clearable
@@ -21,14 +25,31 @@
             >
             </v-file-input>
           </v-col>
-
-         
-          
+        </v-row>
+        <v-row v-if="mapImageUrl">
+          <v-col cols="6" offset="3">
+            <v-row>
+              <v-col cols="6">
+                <v-text-field
+                  label="image width"
+                  v-model="imageWidth"
+                  dark
+                ></v-text-field>
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                  label="image height"
+                  v-model="imageHeight"
+                  dark
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-col>
         </v-row>
         <v-row v-if="mapImageUrl" justify="center">
-            <v-col cols="auto">
-                <v-img :src="mapImageUrl" width="400"></v-img>
-            </v-col>
+          <v-col cols="auto">
+            <v-img ref="imgEl" :src="mapImageUrl" width="400"></v-img>
+          </v-col>
         </v-row>
       </v-container>
     </v-main>
@@ -47,7 +68,7 @@
 <script>
 import navigation from "../../components/nav-bar/nav-bar-component";
 import axios from "axios";
-import { mapGetters, mapMutations } from 'vuex';
+import { mapGetters, mapMutations } from "vuex";
 export default {
   components: {
     navigation,
@@ -57,30 +78,57 @@ export default {
       // imageUrl: "",
       loading: false,
       snackbar: false,
-      msg:''
-
+      msg: "",
     };
   },
   created() {
+    this.setGeoJSON({});
+    this.setPoints([]);
     this.$vuetify.theme.themes.light = {
       primary: "#120543",
       secondary: "#03052C",
       accent: "#3A0473",
     };
   },
-  computed:{
-    ...mapGetters('full-product-store', ['mapImageUrl']),
-    imageUrl:{
-      get(){
-        return this.mapImageUrl
+  computed: {
+    ...mapGetters("full-product-store", [
+      "mapImageUrl",
+      "mapImageHeight",
+      "mapImageWidth",
+    ]),
+    imageUrl: {
+      get() {
+        return this.mapImageUrl;
       },
-      set(newVal){
-        this.setMapImageUrl(newVal) 
-      }
-    }
+      set(newVal) {
+        this.setMapImageUrl(newVal);
+      },
+    },
+    imageWidth: {
+      get() {
+        return this.mapImageWidth;
+      },
+      set(newVal) {
+        this.setMapImageWidth(Number(newVal));
+      },
+    },
+    imageHeight: {
+      get() {
+        return this.mapImageHeight;
+      },
+      set(newVal) {
+        this.setMapImageHeight(Number(newVal));
+      },
+    },
   },
   methods: {
-    ...mapMutations('full-product-store', ['setMapImageUrl']),
+    ...mapMutations("full-product-store", [
+      "setMapImageUrl",
+      "setMapImageHeight",
+      "setMapImageWidth",
+      "setGeoJSON",
+      "setPoints",
+    ]),
     async createImageData(file) {
       this.msg = "";
       if (!file) {
@@ -93,19 +141,20 @@ export default {
       try {
         let res = await axios.post(
           "http://imageupload.mhacd.com/upload.php",
-          formData,
-        //   { headers: { "Access-Control-Allow-Headers": "*" } }
+          formData
+          //   { headers: { "Access-Control-Allow-Headers": "*" } }
         );
         this.loading = false;
         this.imageUrl = res.data;
-        this.msg = res.data.search("not uploaded") === -1 ? 'success ' : '' + res.data;
-        this.snackbar  = true
+        this.msg =
+          res.data.search("not uploaded") === -1 ? "success " : "" + res.data;
+        this.snackbar = true;
       } catch (err) {
         this.loading = false;
         console.log(err);
         console.log(err.message);
         this.msg = err.message;
-        this.snackbar  = true
+        this.snackbar = true;
       }
     },
   },
@@ -118,7 +167,7 @@ export default {
       130deg,
       var(--v-primary-base) 0%,
       var(--v-secondary-base) 100%
-    )
+    );
   }
 }
 </style>
